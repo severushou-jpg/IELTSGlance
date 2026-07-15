d92478d5a3d8eaa5ffcf4d2fb02a57f07dc933a2
severushou-jpg <severushou@outlook.com>
2026-07-16 07:06:10 +0800

    Initial Commit

---
+++ GREGlance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GREGlance
+//
+//  Created by severushou on 2026/7/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GREGlance/GREGlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GREGlanceApp.swift
+//  GREGlance
+//
+//  Created by severushou on 2026/7/16.
+//
+
+import SwiftUI
+
+@main
+struct GREGlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
